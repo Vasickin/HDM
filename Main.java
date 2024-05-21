7b25dcb2904c0641de446b817e9e69a30dd95933
Lex <vasickin@mail.ru>
2024-05-21 04:46:25 +0300

    added products with the temperature parameter

--- Main.java
+++ Main.java
@@ -2,28 +2,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main {
 
     public static void main(String[] args) {
-        Product product1 = new Product(100, "Капучино");
-        Product product2 = new Product(200, "Лате");
-        Product product3 = new Product(300, "Мокачино");
+        // Создание горячих напитков
+        HotDrink drink1 = new HotDrink(100, "Капучино", 70);
+        HotDrink drink2 = new HotDrink(200, "Лате", 75);
+        HotDrink drink3 = new HotDrink(300, "Мокачино", 80);
 
-        VendingMachine vMachin = new VendingMachine();
+        // Создание торгового автомата для горячих напитков
+        HotDrinksMachine hotDrinksMachine = new HotDrinksMachine();
 
+        // Инициализация списка продуктов
         List<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
+        products.add(drink1);
+        products.add(drink2);
+        products.add(drink3);
 
-        vMachin.initProducts(products);
+        // Инициализация продуктов в торговом автомате
+        hotDrinksMachine.initProducts(products);
 
-        Product p = vMachin.getProduct("Мокачино");
-        if (p !=null){
-            System.out.println("Продукт найден: " + p.GetName() + ", Цена: " + p.GetCost());
+        // Получение продукта по имени
+        Product p1 = hotDrinksMachine.getProduct("Мокачино");
+        if (p1 != null) {
+            System.out.println("Продукт найден: " + p1.getName() + ", Цена: " + p1.getCost());
         } else {
             System.out.println("Продукт не найден!");
+        }
+
+        // Получение горячего напитка по имени, объему и температуре
+        HotDrink p2 = hotDrinksMachine.getProduct("Лате", 200, 75);
+        if (p2 != null) {
+            System.out.println("Горячий напиток найден: " + p2.getName() + ", Цена: " + p2.getCost() + ", Температура: " + p2.getTemperature());
+        } else {
+            System.out.println("Горячий напиток не найден!");
         }
     }
 }
