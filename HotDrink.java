8d26914266b7ebb8e5bb630fe966ed170a168ecd
Lex <vasickin@mail.ru>
2024-05-22 21:33:48 +0300

    add documentation for the class HotDrink

--- HotDrink.java
+++ HotDrink.java
@@ -1,21 +1,42 @@
 
+/**
+ * Класс HotDrink
+ *
+ * @author lex
+ */
 public class HotDrink extends Product {
 
-    // Дополнительное поле температуры для горячих напитков
+    /**
+     * Дополнительное поле температуры для горячих напитков
+     */
     private int temperature;
 
-    // Конструктор класса HotDrink
+    /**
+     * Конструктор класса HotDrink
+     *
+     * @param cost
+     * @param name
+     * @param temperature
+     */
     public HotDrink(int cost, String name, int temperature) {
         super(cost, name);
         this.temperature = temperature;
     }
 
-    // Геттер для температуры
+    /**
+     * Геттер для температуры
+     *
+     * @return
+     */
     public int getTemperature() {
         return temperature;
     }
 
-    // Сеттер для температуры
+    /**
+     * Сеттер для температуры
+     *
+     * @param temperature
+     */
     public void setTemperature(int temperature) {
         this.temperature = temperature;
     }
